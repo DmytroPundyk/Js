77764b710719cddc7afbfd85cc39a32b9b885808
DmytroPundyk <dimonpund@gmail.com>
2021-08-19 23:11:02 +0300

    js

--- js/callback.js
+++ js/callback.js
@@ -1,5 +1,5 @@
 
-"use strict"
+
 
 
 // const main = function (a, b, c, callback) {
@@ -42,20 +42,46 @@
 // console.log(counter);
 
 
-const user = {
-   tag: "Mango",
-   showTag() {
-      console.log("showTag => this", this);
-      console.log("showTag => this.tag", this.tag);
+// const user = {
+//    tag: "Mango",
+//    showTag() {
+//       console.log("showTag => this", this);
+//       console.log("showTag => this.tag", this.tag);
 
-   },
+//    },
+// };
+
+// const invokeAction = function (action) {
+//    // console.log(action);
+
+//    action();
+// };
+
+// // console.log(user.showTag);
+// invokeAction(user.showTag.bind(user));
+
+
+//   http://fecore.net.ua/books/m5ph3r-javascript/module-02/scope.html
+//  * http://fecore.net.ua/books/m5ph3r-javascript/module-04/closures.html
+//  * Измените анонимную функцию которая присваивается к result[i]
+//  * так, чтобы в результате в консоли мы послучили числа от 0 до 4 включительно
+//  * Изменять можно только функцию 
+//  * function () {
+//     console.log(i);
+//   };
+
+
+const result = [];
+
+for (var i = 0; i < 5; i++) {
+   (result[i] = function () {
+      console.log(i)
+   }());
 };
 
-const invokeAction = function (action) {
-   // console.log(action);
 
-   action();
-};
-
-// console.log(user.showTag);
-invokeAction(user.showTag);
+result[0](); // 5, ожидаем 0
+result[1](); // 5, ожидаем 1
+result[2](); // 5, ожидаем 2
+result[3](); // 5, ожидаем 3
+result[4](); // 5, ожидаем 4
